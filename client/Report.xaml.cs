26334fc2890247d5476f2deafd82c8a00eaf0ad6
selyavkoivan <selyavkosh2@gmail.com>
2021-12-13 19:05:29 +0300

    create profit order

---
+++ client/Report.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace client
+{
+    public partial class Report : Window
+    {
+        private string location;
+        public Report(string location)
+        {
+            this.location = location;
+            InitializeComponent();
+            report.Text = location;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(report.Text);
+        }
+    }
+}
--- client/profitDiagram.xaml.cs
+++ client/profitDiagram.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Input;
@@ -59,7 +61,67 @@ namespace client
 
         private void Report_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            if (SelectYear.SelectedDate == null)
+            {
+                MessageBox.Show("Выберите дату");
+                return;
+
+            }
+
+            String number = DateTime.Now.Year.ToString() +
+                            DateTime.Now.Month.ToString() +
+                            DateTime.Now.Hour.ToString() +
+                            DateTime.Now.Second.ToString();
+            String reportString = "			ОТДЕЛ ФИНАНСОВ\n" +
+                                  "			ДАТА " + DateTime.Now.ToShortDateString() + "\n\n" +
+                                  "ОТЧЕТ №" + number + "\n\n" +
+                                  "Отчет по прибыли за " + SelectYear.SelectedDate.Value.Year + " год\n\n" +
+                                  "+-------------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+\n" +
+                                  "| Тип         | январь   | февраль  | март     | апрель   | май      | июнь     | июль     | август   | сентябрь | октябрь  | ноябрь   | декабрь  |\n" +
+                                  "+-------------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+\n";
+
+            foreach (var v in this.orders.GroupBy(o => o.product.type))
+            {
+                reportString += $"| {v.Key,-11}";
+                for (int i = 0; i < 12; i++)
+                {
+                    reportString += $" | {" ",-8}";
+                }
+
+                reportString += " |\n";
+                reportString += $"| {"Количество",-11}";
+                for (int i = 0; i < 12; i++)
+                {
+                    reportString += $" | {v.Where(o => o.date.Month == i + 1).Sum(o => o.product.popularity),-8}";
+                }
+
+                reportString += " |\n";
+                reportString += $"| {"Прибыль",-11}";
+                for (int i = 0; i < 12; i++)
+                {
+                    reportString +=
+                        $" | {v.Where(o => o.date.Month == i + 1).Sum(o => o.product.popularity * o.product.price),-8}";
+                }
+
+                reportString += " |\n";
+                reportString += $"| {"Ср. цена",-11}";
+                for (int i = 0; i < 12; i++)
+                {
+                    int count = v.Where(o => o.date.Month == i + 1).Sum(o => o.product.popularity);
+                    double price = v.Where(o => o.date.Month == i + 1).Sum(o => o.product.popularity * o.product.price);
+                    if (count != 0) reportString += $" | {Math.Round(100.0 * price / count) / 100.0,-8}";
+                    else reportString += $" | {0,-8}";
+                }
+
+                reportString += " |\n";
+                reportString +=
+                    "+-------------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+\n";
+            }
+
+        
+            File.WriteAllText(number + ".txt", reportString);
+            new Report(Directory.GetCurrentDirectory() + "\\" + number + ".txt").ShowDialog();
+
         }
     }
 }
